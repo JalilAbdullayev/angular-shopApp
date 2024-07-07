7f11fedb9c3a8c18472413dc42cf00e1cd6306c1
JalilAbdullayev <abdullacelil00@gmail.com>
2024-07-07 08:07:04 +0400

    Categories Menu

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -10,6 +10,8 @@ import {Category} from "../model/category.model";
   styleUrl: './shop.component.css'
 })
 export class ShopComponent {
+  public selectedCategory: Category | undefined | null = null;
+
   constructor(private productRepository: ProductRepository, private categoryRepository: CategoryRepository) {
   }
 
@@ -19,5 +21,9 @@ export class ShopComponent {
 
   get categories(): Category[] {
     return this.categoryRepository.getCategories();
+  }
+
+  changeCategory(newCategory?: Category) {
+    this.selectedCategory = newCategory;
   }
 }
