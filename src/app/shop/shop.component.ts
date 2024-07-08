9ff5695757f243eb1fd0efde21524853bbaa8420
JalilAbdullayev <abdullacelil00@gmail.com>
2024-07-08 09:51:08 +0400

    Pagination Links

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -22,6 +22,17 @@ export class ShopComponent {
     return this.productRepository.getProducts(this.selectedCategory).slice(index, index + this.productsPerPage);
   }
 
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.productRepository
+      .getProducts(this.selectedCategory).length / this.productsPerPage))
+      .fill(0)
+      .map((a, i) => i + 1);
+  }
+
+  changePage(p: number) {
+    this.selectedPage = p;
+  }
+
   get categories(): Category[] {
     return this.categoryRepository.getCategories();
   }
