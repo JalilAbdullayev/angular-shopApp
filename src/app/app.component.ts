442e6c23125704930631defede66001d2478464e
JalilAbdullayev <abdullacelil00@gmail.com>
2024-09-19 15:13:44 +0400

    feat: Add Bootstrap, JSON Server and JSON Web Token dependencies
    Add Bootstrap and JSON Server, JSON Web Token dependencies.
    Generate admin, model and shop modules.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'root',
+  template: `
+    <div class="bg-primary m-5 p-5 text-white">
+      Shop Application
+    </div>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'shopApp';
+  title = 'Shop Application';
 }
