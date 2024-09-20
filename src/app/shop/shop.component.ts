6010d267e2f18eb87102d50696ec5d4ba56729f4
JalilAbdullayev <abdullacelil00@gmail.com>
2024-09-20 16:45:54 +0400

    feat(shop): implement product pagination
    Add pagination to the shop component, allowing users to browse products in smaller chunks. This improves the user experience by reducing the amount of data loaded at once and making it easier to find specific products.

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -11,12 +11,15 @@ import {Category} from "../model/category.model";
 })
 export class ShopComponent {
   public selectedCategory: Category = null;
+  public productsPerPage: number = 3;
+  public selectedPage: number = 1;
 
   constructor(private productRepository: ProductRepository, private categoryRepository: CategoryRepository) {
   }
 
   get products(): Product[] {
-    return this.productRepository.getProducts(this.selectedCategory);
+    let index = (this.selectedPage - 1) * this.productsPerPage;
+    return this.productRepository.getProducts(this.selectedCategory).slice(index, index + this.productsPerPage);
   }
 
   get categories(): Category[] {
