1fbe649c13c353077a5c6fb07c1093c478e2a4d5
JalilAbdullayev <abdullacelil00@gmail.com>
2024-09-21 18:19:29 +0400

    Design checkout component

--- src/app/shop/checkout/checkout.component.ts
+++ src/app/shop/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Order} from "../../model/order.model";
+import {OrderRepository} from "../../model/order.repository";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'checkout',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+  orderSent: boolean = false;
+  submitted: boolean = false;
 
+  constructor(public order: Order, private orderRepository: OrderRepository) {
+  }
+
+  submitOrder(form: NgForm) {
+    this.submitted = true;
+    if (form.valid) {
+      this.orderRepository.saveOrder(this.order).subscribe(order => {
+        this.order.clearOrder();
+        this.orderSent = true;
+        this.submitted = false;
+      });
+    }
+  }
 }
