dcf72698a9ba825d0101d331906ddb45c87e973d
JalilAbdullayev <abdullacelil00@gmail.com>
2024-09-22 05:43:13 +0400

    Generate admin module with routing
    Add admin link to the navbar
    Generate admin & auth component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
+import {AdminRoutingModule} from './admin-routing.module';
+import {AdminComponent} from './admin.component';
+import {AuthComponent} from './auth/auth.component';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        AdminComponent,
+        AuthComponent
+    ],
+    imports: [
+        CommonModule,
+        AdminRoutingModule
+    ]
 })
-export class AdminModule { }
+export class AdminModule {
+}
