0c7b3d4d140ffb01be81cb8aa0bbd8cd2a0e1415
JalilAbdullayev <abdullacelil00@gmail.com>
2024-09-22 17:30:39 +0400

    Design product form

--- src/app/admin/products/product-form/product-form.component.ts
+++ src/app/admin/products/product-form/product-form.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Product} from "../../../model/product.model";
+import {ProductRepository} from "../../../model/product.repository";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-product-form',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './product-form.component.css'
 })
 export class ProductFormComponent {
+  edit: boolean = false;
+  product: Product = new Product();
 
+  constructor(private activeRoute: ActivatedRoute, private repository: ProductRepository) {
+    this.edit = activeRoute.snapshot.params['mode'] === 'edit';
+    if (this.edit) {
+      this.product = repository.getProduct(activeRoute.snapshot.params['id']);
+    }
+  }
+
+  save(form: NgForm) {
+
+  }
 }
